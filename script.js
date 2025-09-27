ca1614ff6e710f4b733e3c47409334497bffcce5
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-09-27 20:43:24 +0330

    taskName

--- script.js
+++ script.js
@@ -22,7 +22,15 @@ const addTask = () => {
   const task=`<div class="task">
    <input type="checkbox" class="task-check">
    <span class="taskname">${taskName}</span>
-   <button class="edit"></button>
-   <button class="delete"></button>
+   <button class="edit">
+   <i class="fa-solid fa-pen-to-square"></i>
+   </button>
+   <button class="delete">
+    <i class="fa-solid fa-trash"></i>
+   </button>
   </div>`
+
+  tasksContainer.insertAdjacentHTML("beforeend",task);
+
 };
+addBtn.addEventListener("click",addTask)
